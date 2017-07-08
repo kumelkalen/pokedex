e4814d521ccb17d2612dda306b371f0af5ea7783
kumelkalen <free.your.mind.fc@gmail.com>
2017-07-08 03:08:40 -0400

    Definimos el tipo de respuesta de ajax

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
 $(document).ready(function($){
-	
+	$.ajax({
+		url: 'http://pokeapi.co/api/v2/pokemon/', //Link datos pokemones
+		type: 'GET', // Pedimos obtener datos
+		dataType: 'JSON', // Definimos tipo de dato
+		data:{"limit": '10'}, // Lo limitamos a que se emuestren de 10;
+	})
+
 });
