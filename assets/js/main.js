264869e326271a518490a3d5c8fde726d4ccbc93
kumelkalen <free.your.mind.fc@gmail.com>
2017-07-08 04:37:13 -0400

    Ponemos las imágenes y el nombre de cada pokemon que aparezca

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,14 +15,15 @@ $(document).ready(function($){
 	.always(function(){
 		console.log("complete");
 	})
-	function mostrar (data){ //Recorremos la parte "results" del objeto
-		data.results.forEach(function(pokeDatos){
+	function mostrar(data){ //Recorremos la parte "results" del objeto
+		data.results.forEach(function(pokeDatos){ 
 			var habilidades = pokeDatos.abilities; //array
 			var alto = pokeDatos.height;
 			var peso = pokeDatos.weight;
 			var nombre = pokeDatos.name;
 			var id = pokeDatos.id;
-			
-		})
+		$("#poke-lista").append("<div class='cont-poke col-md-2'><img class='imagen' src='http://img.pokemondb.net/artwork/"+ nombre + ".jpg'><h4>"+ nombre +"</h4></div>");
+
+		});
 	}
 });
